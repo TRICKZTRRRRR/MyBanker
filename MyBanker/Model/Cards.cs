3cceca3c5bbc50a2be64eaf90f278856c45281c0
Jonas Steinmejer <jonas04@outlook.dk>
2024-06-11 12:04:22 +0200

    Updated Cards class

--- MyBanker/Model/Cards.cs
+++ MyBanker/Model/Cards.cs
@@ -7,27 +7,12 @@ using System.Threading.Tasks;
 
 namespace MyBanker.Model
 {
-    internal class Cards
+    public abstract class Cards
     {
-        private string _cardholdersName { get; set; }
-        private string _cardType { get; set; }
-        private string _cardNumber { get; set; }
-        private DateTime _expirationDate { get; set; }
-        private string _accountNumber { get; set; }
-
-        public Cards(string cardholdersName, string cardType, string cardNumber, DateTime expirationDate, string accountNumber)
-        {
-            _cardholdersName = cardholdersName;
-            _cardType = cardType;
-            _cardNumber = cardNumber;
-            _expirationDate = expirationDate;
-            _accountNumber = accountNumber;
-        }
-        
-        public string CardholdersName => _cardholdersName;
-        public string CardType => _cardType;
-        public string CardNumber => _cardNumber;
-        public DateTime ExpirationDate => _expirationDate;
-        public string AccountNumber => _accountNumber;
+        public string CardholdersName { get; set; }
+        public string CardType { get; set; }
+        public string CardNumber { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string AccountNumber { get; set; }
     }
 }
