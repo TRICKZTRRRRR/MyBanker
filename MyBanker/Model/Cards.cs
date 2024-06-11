1274287412eaa4ff71af0fa22ce3d53bea470c21
Jonas Steinmejer <jonas04@outlook.dk>
2024-06-11 12:01:11 +0200

    Added Cards class and properties

---
+++ MyBanker/Model/Cards.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBanker.Model
+{
+    internal class Cards
+    {
+        private string _cardholdersName { get; set; }
+        private string _cardType { get; set; }
+        private string _cardNumber { get; set; }
+        private DateTime _expirationDate { get; set; }
+        private string _accountNumber { get; set; }
+
+        public Cards(string cardholdersName, string cardType, string cardNumber, DateTime expirationDate, string accountNumber)
+        {
+            _cardholdersName = cardholdersName;
+            _cardType = cardType;
+            _cardNumber = cardNumber;
+            _expirationDate = expirationDate;
+            _accountNumber = accountNumber;
+        }
+        
+        public string CardholdersName => _cardholdersName;
+        public string CardType => _cardType;
+        public string CardNumber => _cardNumber;
+        public DateTime ExpirationDate => _expirationDate;
+        public string AccountNumber => _accountNumber;
+    }
+}
